d1144d9c3d3eded54610f3dfee22f35a3f53e64b
Monica Grages <mgrages1@gmail.com>
2018-01-21 18:54:55 -0500

    setting up photo gallery views

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,5 +1,15 @@
 class PhotosController < ApplicationController
 	def index
 		@gallery_images = Photo.all
+		# will break up by type here if requested
+	end
+
+	def new
+	end
+
+	def create
+	end
+
+	def destroy
 	end
 end
